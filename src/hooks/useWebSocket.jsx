00cca8b74e950c54264d21a872adfd52d429c5b4
Bhargav <bhargav.arvapalli@cloudwick.com>
2026-03-02 23:54:04 +0530

    updated endpoint

--- src/hooks/useWebSocket.jsx
+++ src/hooks/useWebSocket.jsx
@@ -2,8 +2,7 @@
 import { useEffect, useRef } from "react";
 import useWebSocketStore from "../store/websocketStore";
 
-const WS_URL =
-  import.meta.env.VITE_WS_URL || "wss://riskmonitorapi2232.duckdns.org:8000/ws";
+const WS_URL = "wss://riskmonitorapi2232.duckdns.org/ws/"; 
 
 const useWebSocket = (enabled = true) => {
   const wsRef = useRef(null);
